d073a7440f122bd57afdb285572f60dcd42ec323
huy19712011 <35172019+huy19712011@users.noreply.github.com>
2025-11-04 19:27:20 +0700

    Set up basic spring boot project

---
+++ src/main/java/org/example/java6nfa25sd20203/Java6NFa25Sd20203Application.java
@@ -0,0 +1,15 @@
+package org.example.java6nfa25sd20203;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Java6NFa25Sd20203Application {
+
+    public static void main(String[] args) {
+
+        SpringApplication.run(Java6NFa25Sd20203Application.class, args);
+        System.out.println("running...");
+    }
+
+}
---
+++ src/test/java/org/example/java6nfa25sd20203/Java6NFa25Sd20203ApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.java6nfa25sd20203;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Java6NFa25Sd20203ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
