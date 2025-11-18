c176cffc5a7461245b6c92f754b7e6756650b390
huy19712011 <35172019+huy19712011@users.noreply.github.com>
2025-11-18 17:40:59 +0700

    DTO: Book, BookRequest, BookResponse - part 2

--- src/main/java/org/example/java6nfa25sd20203/controller/BookController.java
+++ src/main/java/org/example/java6nfa25sd20203/controller/BookController.java
@@ -1,11 +1,51 @@
 package org.example.java6nfa25sd20203.controller;
 
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import jakarta.validation.Valid;
+import lombok.RequiredArgsConstructor;
+import org.example.java6nfa25sd20203.dto.BookRequest;
+import org.example.java6nfa25sd20203.dto.BookResponse;
+import org.example.java6nfa25sd20203.service.BookService;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/api/books")
+@RequiredArgsConstructor
 public class BookController {
 
+    private final BookService bookService;
 
+    @GetMapping
+    public ResponseEntity<List<BookResponse>> getAllBooks() {
+
+        return ResponseEntity.status(HttpStatus.OK).body(bookService.findAll());
+    }
+
+    @GetMapping("id")
+    public ResponseEntity<BookResponse> getBookById(@PathVariable("id") long id) {
+
+        return ResponseEntity.status(HttpStatus.OK).body(bookService.findById(id));
+    }
+
+    @PostMapping
+    public ResponseEntity<BookResponse> add(@Valid @RequestBody BookRequest bookRequest) {
+
+        return ResponseEntity.status(HttpStatus.CREATED).body(bookService.add(bookRequest));
+    }
+
+    @PutMapping("id")
+    public ResponseEntity<BookResponse> update(@Valid @RequestBody BookRequest bookRequest, @PathVariable("id") long id) {
+
+        return  ResponseEntity.status(HttpStatus.OK).body(bookService.update(bookRequest, id));
+    }
+
+    @DeleteMapping("id")
+    public ResponseEntity<Void> delete(@PathVariable("id") long id) {
+
+        bookService.delete(id);
+        return ResponseEntity.noContent().build();
+    }
 }
