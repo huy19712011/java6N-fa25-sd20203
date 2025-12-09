ef91def0c5f6049ecf5f0d61fb83d3b425a4b2bc
huy19712011 <35172019+huy19712011@users.noreply.github.com>
2025-12-09 18:09:50 +0700

    Security: jwt demo

--- src/main/java/org/example/java6nfa25sd20203/config/SpringSecurityConfig.java
+++ src/main/java/org/example/java6nfa25sd20203/config/SpringSecurityConfig.java
@@ -16,7 +16,7 @@ import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
 
-@Configuration
+//@Configuration
 @RequiredArgsConstructor
 public class SpringSecurityConfig {
 
